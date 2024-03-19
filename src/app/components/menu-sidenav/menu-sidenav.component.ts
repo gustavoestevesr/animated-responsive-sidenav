fe86c32806316dd0cdc329ea90ed40b75c913d41
gustavoesteves <gustavoesteves@directon.com.br>
2024-03-19 20:09:15 -0300

    feat: active route

--- src/app/components/menu-sidenav/menu-sidenav.component.ts
+++ src/app/components/menu-sidenav/menu-sidenav.component.ts
@@ -3,12 +3,13 @@ import { Component, Input } from '@angular/core';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 export type MenuItem = {
   icon: string;
   label: string;
   route?: string;
+  isActive: boolean;
 }
 
 @Component({
@@ -20,7 +21,8 @@ export type MenuItem = {
 })
 export class MenuSidenavComponent {
 
-  constructor(public router: Router) {}
+  constructor(private router: Router) {}
+
 
   @Input() collapsed: boolean = false;
 
@@ -29,26 +31,44 @@ export class MenuSidenavComponent {
       icon: 'dashboard',
       label: 'Dashboard',
       route: 'dashboard',
+      isActive: true,
     },
     {
       icon: 'video_library',
       label: 'Content',
       route: 'content',
+      isActive: false,
     },
     {
       icon: 'analytics',
       label: 'Analytics',
       route: 'analytics',
+      isActive: false,
     },
     {
       icon: 'comment',
       label: 'Comments',
       route: 'comments',
+      isActive: false,
     }
   ];
 
   get profilePicSize() {
     return this.collapsed ? '32' : '100';
+  }
+
+  navigateTo(item: MenuItem) {
+    this.activePage(item)
+    this.router.navigate([item.route])
+  }
+
+  activePage(itemTarget: MenuItem) {
+    this.menuItems.forEach((item) => {
+      item.isActive = false;
+      if(item.label === itemTarget.label) {
+        item.isActive = true;
+      }
+    })
   }
 
 }
